f7801cd4706d1a6f6ddff0c7770392abd9e5fc5c
Ali95800 <ali.c95800@gmail.com>
2021-11-22 16:42:20 +0100

    exo 22/11/2021

--- Js/exercice/10_nb_jour.js
+++ Js/exercice/10_nb_jour.js
@@ -1,7 +1,11 @@
-var date = new Date();
-var listeNbrJours = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-var mois = date.getMonth();
-var nbJours = listeNbrJours[mois];
-if (isAnneeBisextile(date.getYear())) {
-  nbJours++;
+var mois = parseInt (prompt("Sélectionnez votre mois", "saisir un chiffre entre 1 et 12"));
+
+if(mois = 2){
+    document.write(" Il y à 28 jours ce mois ci !")
+} else if((mois === 4)|| (mois === 6)|| (mois === 9)|| (mois === 11)){
+    document.write(" ce moit fait 30jours")
+}else if((mois === 1)|| (mois === 3)|| (mois === 7)|| (mois === 18) || (mois12)){
+    document.write(" ce moit fait 31jours")
+}else{
+    alert ("saisie hors calendier")
 }
