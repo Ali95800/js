d5e8688cd1729951bfafcc2139c71177e1e09268
Ali95800 <ali.c95800@gmail.com>
2021-12-06 16:27:36 +0100

    js du 06/12/2021

---
+++ Js/exercice/12_manege.js
@@ -0,0 +1,12 @@
+var manege = 0
+while(manege < 10){
+    manege ++;
+    document.write(" c'est le tour n° " + manege + "<br>");
+}
+document.write("<hr>");
+
+var question = parseInt (prompt("Combien de tours voulez-vous faire?"));
+var compteur = 1;
+while(compteur <=){
+    document.write
+}
